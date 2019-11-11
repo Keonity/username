a1c075142183d6e1745ca017ff426ce27e666806
Keoni Lanoza <lanoza@chapman.edu>
2019-11-10 16:52:20 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Keoni Lanoza
 echo "Enter a username: "
 read username
-while echo "$username" | egrep -v "^[a-z][a-z,0-9,_]{2,11}$" > /dev/null 2>&1
+while echo "$username" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - 3-12 characters, alphanumeric characters, and underscores only!"
 	echo "Enter a username: "
